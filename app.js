a130ad2988aa65745893932b92550715a1e1f86b
Gabriel Machado <glemosmachado76@gmail.com>
2025-08-04 22:10:08 -0300

    adjustments to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connection to MongoDB'))
+.catch((err) => console.error('MongoDB error', err));
 
 app.get('/', (req, res) =>{
     res.send('Test')
